37debd47b5d60f4929798ffcfe3b57dd6f0dcda1
ZHADKO MYKYTA <134333337+nikk0308@users.noreply.github.com>
2023-10-17 00:17:10 +0300

    Added play with friends

--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -4,10 +4,10 @@ class Program {
     static void Main(string[] args) {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         var enc1251 = Encoding.GetEncoding(1251);
- 
+
         System.Console.OutputEncoding = System.Text.Encoding.UTF8;
         System.Console.InputEncoding = enc1251;
-        
+
         App app = new App();
         app.Run();
     }
@@ -72,6 +72,59 @@ class MainMenu {
 
     internal void PlayWithFriends() {
         Console.WriteLine("*гра з друзями*");
+
+        GamePlay game = new GamePlay();
+        bool isContinue = true;
+        int playersAmount = 0;
+        Player[] players = new Player[0];
+        string choice;
+        string playerName;
+
+        Console.WriteLine("Ви можете додати від 2 до 4 гравців.");
+        while (isContinue) {
+            Console.Write("Введіть 1, якщо хочете додати друга для гри або 0, якщо більше не хочете: ");
+            choice = Console.ReadLine();
+            switch (choice) {
+                case "1":
+                    int arrayLength = players.Length;
+                    Player[] newPlayers = new Player[arrayLength + 1];
+                    for (int i = 0; i < arrayLength; i++) {
+                        newPlayers[i] = players[i];
+                    }
+
+                    players = newPlayers;
+                    Console.Write($"Введіть ім'я гравця під номером {playersAmount + 1}: ");
+                    playerName = Console.ReadLine();
+                    players[arrayLength] = new Player(playerName, chipColor: (ConsoleColor)(arrayLength + 1));
+                    playersAmount++;
+                    if (playersAmount == 4) {
+                        isContinue = false;
+                    }
+
+                    break;
+                case "0":
+                    if (playersAmount < 2) {
+                        Console.WriteLine("У грі мають брати участь не менше 2 людей, додайте гравців");
+                    }
+                    else {
+                        isContinue = false;
+                    }
+
+                    break;
+                default:
+                    Console.WriteLine("Спробуйте ще раз ^_^");
+                    break;
+            }
+        }
+
+        ConsoleColor defaltColor = Console.ForegroundColor;
+        Console.WriteLine("Чудово! Ось ваш список гравців:");
+        for (int i = 0; i < players.Length; i++) {
+            Console.Write($"Гравець {i + 1}: ");
+            Console.ForegroundColor = players[i].chipColor;
+            Console.WriteLine(players[i].nameInGame);
+            Console.ForegroundColor = defaltColor;
+        }
     }
 
     internal void ChangeSettings() {
@@ -101,7 +154,7 @@ class Player {
     public bool isInPrison;
     public int turnsToGoOutOfPrison;
 
-    public Player(string nameInGame = "default", int moneyAmount = 0, Position positionInField = new Position(),
+    public Player(string nameInGame, int moneyAmount = 0, Position positionInField = new Position(),
         ConsoleColor chipColor = ConsoleColor.White, bool isInPrison = false, int turnsToGoOutOfPrison = 0) {
         this.nameInGame = nameInGame;
         this.moneyAmount = moneyAmount;
@@ -136,6 +189,7 @@ class GamePlay {
             }
         }
     }
+
     public int RollDice() {
         return _rand.Next(1, 7);
     }
@@ -147,33 +201,33 @@ class GamePlay {
 
 class Enterprise {
     public Player? owner;
-    
+
     private readonly int priceToBuy;
     private readonly int priceToBuildHotel;
     private readonly int priceToPawnToBank;
     private readonly int priceToSellToBank;
-    
+
     public bool isPawnedInBank;
     public int turnsToDisappearIfPawned;
-    
+
     public bool isBuiltHotel;
     private readonly int priceOthersPayLevel1;
     private readonly int priceOthersPayLevel2;
     private readonly int priceOthersPayLevel3;
     public int currentPriceOthersPay;
 
-    public Enterprise(int priceToBuy, Player? owner = null, bool isPawnedInBank = false, 
+    public Enterprise(int priceToBuy, Player? owner = null, bool isPawnedInBank = false,
         int turnsToDisappearIfPawned = 0, bool isBuiltHome = false) {
         this.owner = owner;
-        
+
         this.priceToBuy = priceToBuy;
         priceToBuildHotel = priceToBuy * 3;
         priceToPawnToBank = priceToBuy / 2;
         priceToSellToBank = priceToBuy;
-        
+
         this.isPawnedInBank = isPawnedInBank;
         this.turnsToDisappearIfPawned = turnsToDisappearIfPawned;
-        
+
         isBuiltHotel = isBuiltHome;
         priceOthersPayLevel1 = priceToBuy / 2;
         priceOthersPayLevel2 = priceToBuy;
@@ -193,4 +247,3 @@ class Industry {
         this.isFull = isFull;
     }
 }
-
